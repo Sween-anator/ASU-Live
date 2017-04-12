ef784b817dd03c678f864052b6a903d80b5b5334
ksweeney <sweenkeagan@gmail.com>
2017-04-12 14:19:23 -0700

    init commit

---
+++ packages/vulcan-newsletter/lib/client.js
@@ -0,0 +1,6 @@
+import Newsletters from './collection.js';
+import './emails.js';
+import './custom_fields.js';
+import './fragments.js';
+
+export default Newsletters;
---
+++ packages/vulcan-newsletter/lib/server.js
@@ -0,0 +1,13 @@
+import Newsletters from './collection.js';
+import './emails.js';
+import './custom_fields.js';
+import './fragments.js';
+
+import './server/newsletter.js';
+import './server/mailchimp/mailchimp_newsletter.js';
+import './server/cron.js';
+import './server/emails.js';
+import './server/mutations.js';
+import './server/callbacks.js';
+
+export default Newsletters;
